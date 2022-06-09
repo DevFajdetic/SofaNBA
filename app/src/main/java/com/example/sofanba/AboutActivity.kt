9c2ea87e1da29d9d1a434c67ece1c3c5f648be81
Kristijan Fajdetić <kfajdetic@riteh.hr>
2022-06-09 15:30:32 +0200

    About activity and fragment

---
+++ app/src/main/java/com/example/sofanba/AboutActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sofanba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.sofanba.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/example/sofanba/utils/DateHelper.kt
@@ -0,0 +1,4 @@
+package com.example.sofanba.utils
+
+class DateHelper {
+}
